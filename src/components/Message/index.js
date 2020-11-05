95d5df4c22f3bf4ce8e980acf8eb65a1c5e1eb7e
cecile <cecile@oclock.io>
2020-11-05 15:11:26 +0100

    Correction du bug : utilisation d'une fonction de nettoyage avec useEffect :muscle:

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -7,11 +7,23 @@ const Message = ({ message, hideMessage }) => {
     console.log('Le composant Message apparaît');
 
     // je veux programmer la disparition du composant dans 10 secondes
-    setTimeout(() => {
+    // on peut récupérer l'id du timer pour pouvoir l'annuler
+    const timeoutId = setTimeout(() => {
+      console.log('Le message va disparaître');
       // je demande à App de faire disparaître le message
       hideMessage();
     }, 10000);
-  }, []);
+
+    // quand le message change, je dois annuler le timer avant d'en lancer un nouveau
+    // en fait, je veux "nettoyer" le résultat de l'effet précédent
+    // https://reactjs.org/docs/hooks-reference.html#cleaning-up-an-effect
+    // si on retourne une fonction dans useEffect, c'est une fonction de nettoyage
+    // elle sera appelée automatiquement avant l'application du prochain effet
+    return () => {
+      console.log('on annule le timer');
+      clearTimeout(timeoutId);
+    };
+  }, [message]);
 
   return (
     <MessageUI>{message}</MessageUI>
