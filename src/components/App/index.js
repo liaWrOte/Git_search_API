782983ebf323702e599571517acc76025ccf2f8a
cecile <cecile@oclock.io>
2020-11-05 13:38:08 +0100

    On fait appel à l'API pour récupérer les résultats de la recherche :tada: (et on en profite pour gérer l'affichage du nombre de résultats)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -7,9 +8,6 @@ import 'semantic-ui-css/semantic.min.css';
 import SearchBar from 'src/components/SearchBar';
 import Message from 'src/components/Message';
 import ReposResults from 'src/components/ReposResults';
-
-// import des données
-import reposData from 'src/data/repos';
 
 // si j'utilise une image qui est dans src, je l'importe pour que webpack puisse
 // gérer l'image et avoir un chemin dist/......
@@ -21,8 +19,25 @@ const App = () => {
   // valeur de l'input pour la recherche
   const [search, setSearch] = useState('');
 
+  // repositories à afficher
+  const [repos, setRepos] = useState([]);
+
+  // message à afficher
+  const [message, setMessage] = useState('Bienvenue !');
+
   const makeSearch = () => {
     console.log('c\'est le moment de faire la recherche pour : ', search);
+
+    // on fait appel à l'API
+    axios.get(`https://api.github.com/search/repositories?q=${search}`)
+      .then((response) => {
+        // console.log(response.data);
+        setRepos(response.data.items);
+        setMessage(`La recherche a retourné ${response.data.total_count} résultats`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -31,8 +46,8 @@ const App = () => {
         <img src={logo} alt="" />
       </header>
       <SearchBar manageSubmit={makeSearch} search={search} setSearch={setSearch} />
-      <Message />
-      <ReposResults repos={reposData.items} />
+      <Message message={message} />
+      <ReposResults repos={repos} />
     </div>
   );
 };
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Message as MessageUI } from 'semantic-ui-react';
 
-const Message = () => (
-  <MessageUI>153 résultats</MessageUI>
+const Message = ({ message }) => (
+  <MessageUI>{message}</MessageUI>
 );
+
+Message.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Message;
